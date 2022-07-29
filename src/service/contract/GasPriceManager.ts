c033fc0cb6bd86d5ce233ba382d990853776aae6
mukeunkim <mukeunkim@gmail.com>
2022-07-29 15:17:17 +0900

    GasPriceManager: Change the gas fee calculation according to EIP-1559

--- src/service/contract/GasPriceManager.ts
+++ src/service/contract/GasPriceManager.ts
@@ -2,7 +2,6 @@
 
 import "@nomiclabs/hardhat-web3";
 import { BigNumber, ethers } from "ethers";
-import * as hre from "hardhat";
 
 export class GasPriceManager extends ethers.Signer {
     readonly signer: ethers.Signer;
@@ -38,9 +37,24 @@ export class GasPriceManager extends ethers.Signer {
     sendTransaction(
         transaction: ethers.utils.Deferrable<ethers.providers.TransactionRequest>
     ): Promise<ethers.providers.TransactionResponse> {
-        return hre.web3.eth.getGasPrice().then((gas_price: string) => {
-            const received_gas_price = BigNumber.from(gas_price);
-            transaction.gasPrice = received_gas_price.add(received_gas_price.div(10).mul(5));
+        const provider = this.signer.provider;
+        if (provider === undefined) {
+            const maxPriorityFeePerGas = 1500000000;
+            const maxFeePerGas = maxPriorityFeePerGas;
+
+            transaction.maxPriorityFeePerGas = BigNumber.from(maxPriorityFeePerGas);
+            transaction.maxFeePerGas = BigNumber.from(maxFeePerGas);
+            return this.signer.sendTransaction(transaction).then((tx) => {
+                return tx;
+            });
+        }
+        return provider.getBlock("latest").then((block) => {
+            const baseFeePerGas = block.baseFeePerGas != null ? block.baseFeePerGas.toNumber() : 0;
+            const maxPriorityFeePerGas = 1500000000;
+            const maxFeePerGas = Math.floor(baseFeePerGas * 1.265625) + maxPriorityFeePerGas;
+
+            transaction.maxPriorityFeePerGas = BigNumber.from(maxPriorityFeePerGas);
+            transaction.maxFeePerGas = BigNumber.from(maxFeePerGas);
             return this.signer.sendTransaction(transaction).then((tx) => {
                 return tx;
             });
